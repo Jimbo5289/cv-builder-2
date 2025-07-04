754c4391fe6579f2221599def5cf895bf0b17b0c
James Ingleton <james_ingleton@hotmail.com>
2025-07-04 19:00:21 +0100

    Adjust sidebar top positioning to 50px
    - Changed sidebar top position from 0px to 50px
    - Accounts for any browser chrome or navigation elements
    - Prevents sidebar header from being clipped
    - User-requested positioning adjustment

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -95,7 +95,7 @@ const AdminLayout = ({ children }) => {
           sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0 flex flex-col`}
         style={{ 
-          top: '0px', 
+          top: '50px', 
           marginTop: '0px', 
           paddingTop: '0px',
           height: '100vh'
